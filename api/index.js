3c65bd5070ed05f59366bab2d8a4f212ba299756
Basagoud <basagouda161@gmail.com>
2025-10-19 00:26:43 +0530

    created signup route and tested with insomnia

--- api/index.js
+++ api/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
-
 const app = express();
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ MongoDB connection error:", err));
+
 app.listen(3000, () => {
   console.log("✅ Server is running on port 3000");
 });
+
+app.use("/api/auth",authRouter);
