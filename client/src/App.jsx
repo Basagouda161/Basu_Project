b056e83741431a5b86ff57501de16722928f377d
Basagoud <basagouda161@gmail.com>
2025-10-21 19:19:24 +0530

    private route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Profile from './pages/profile';
 import Header from './components/Header';
-
+import PrivateRoute from './components/PrivateRoute';
 export default function App(){
   return<BrowserRouter>
     <Header/>
@@ -14,8 +14,9 @@ export default function App(){
       <Route path="/About" element={<About/>} />
       <Route path="/Signin" element={<Signin/>} />
       <Route path="/Signup" element={<Signup/>} />
-      <Route path="/Profile" element={<Profile/>} />
-
+      <Route element={<PrivateRoute/>}>
+        <Route path="/Profile" element={<Profile/>} />
+      </Route>
     </Routes>
   </BrowserRouter>;
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import{FaSearch} from 'react-icons/fa'
 import{Link} from 'react-router-dom'
-
+import { useSelector } from 'react-redux'
 export default function Header() {
+    const {currentUser}=useSelector(state=>state.user);
   return (
     <header className='bg-slate-200'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -24,8 +25,10 @@ export default function Header() {
                 <Link to="/"> 
                 <li className='hover:underline'> Home </li>
                 </Link>
-                <Link to="Signin"> 
-                <li className='hover:underline'>Signin</li>
+                <Link to="profile"> 
+                {
+                    currentUser?(  <img className='rounded-full h-7 w-7 object-cover'  src="currentUser.avatar" alt="profile" />):(<li className='hover:underline'>Signin</li>)
+                }
                 </Link>
             </ul>
         </div>
